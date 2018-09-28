31dd0db9176588f0e8fd31f8f0b3b6a2d604e1d4
이규원 <trq1.lee@gmail.com>
2018-09-28 20:30:56 +0900

    DB 테이블 변경

--- docs/board.sql
+++ docs/board.sql
@@ -1,10 +1,11 @@
 CREATE TABLE `board` (
-    `id`    int(20) NOT NULL AUTO_INCREMENT,
+    `id`    int(100) NOT NULL AUTO_INCREMENT,
     `author`    tinytext    NOT NULL,
     `todate`    datetime    NOT NULL    DEFAULT '0000-00-00 00:00:00',
     `modidate`    datetime   NULL    DEFAULT '0000-00-00 00:00:00',
-	  `title`  varchar(20) NULL,
+	  `title`  varchar(300) NULL,
     `content`   text     NULL,
-    `passwd`  varchar(20) NULL,
+    `passwd`  varchar(30) NULL,
+    `parent` int(100) NULL,
     primary key(id)
 );
