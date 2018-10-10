1f302d12c6b162bb0a8df2b452c3d634d331ed71
이규원 <trq1.lee@gmail.com>
2018-10-10 17:20:54 +0900

    board 테이블 변경(덧글처리)

--- docs/board.sql
+++ docs/board.sql
@@ -1,11 +1,22 @@
 CREATE TABLE `board` (
     `id`    int(100) NOT NULL AUTO_INCREMENT,
-    `author`    varchar(50)    NOT NULL,
+    `author`    tinytext    NOT NULL,
     `todate`    datetime    NOT NULL    DEFAULT '0000-00-00 00:00:00',
     `modidate`    datetime   NULL    DEFAULT '0000-00-00 00:00:00',
-	  `title`  varchar(300) NULL,
+  	`title`  varchar(300) NULL,
     `content`   text     NULL,
     `passwd`  varchar(30) NULL,
     `parent` int(100) NULL,
+    `type` varchar(20) NULL,
+    `indent` int(100) NULL DEFAULT 0,
+    `step` int(100) NULL DEFAULT 0,
+    `sort` int(100) NULL DEFAULT 0,
+    primary key(id)
+);
+
+CREATE TABLE `account` (
+	`id` int (100) NOT NULL AUTO_INCREMENT,
+    `userid` varchar(15) NOT NULL,
+    `userpasswd` varchar(30) NOT NULL,
     primary key(id)
 );
